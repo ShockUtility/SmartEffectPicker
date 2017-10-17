119c8993587386daa065c3690d1d0cbeee6215b7
shock <shock@docs.kr>
2017-10-18 03:18:18 +0900

    Initial commit

---
+++ Example/SmartEffectPicker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SmartEffectPicker
+//
+//  Created by ShockUtility on 10/18/2017.
+//  Copyright (c) 2017 ShockUtility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
